2d661ddfc24620f28af7b7257e81c92a7d289bac
William Oliveira de Lagos <william.lagos@live.com>
2013-07-29 19:07:24 -0300

    First commit for Fabrica

---
+++ README.md
@@ -0,0 +1,4 @@
+cms-bootstrap
+=============
+
+Django CMS Bootstrap Template
---
+++ project/production.py
@@ -0,0 +1,12 @@
+from settings import *
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'd7274m55nb4lvp',
+    'HOST': 'ec2-23-21-129-229.compute-1.amazonaws.com',
+    'PORT': 5432,
+    'USER': 'zcjghyllycxehe',
+    'PASSWORD': 'oEjmdYtYj1yk0-D5gaOplQhcLH'
+  }
+}
