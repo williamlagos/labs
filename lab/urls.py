913b94185016adbe531cc98527237bb9a6c4e53a
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-02 19:03:31 -0300

    Removed unused dependencies and updated requirements

--- lab/urls.py
+++ lab/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django_distill import distill_url
-from lab.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 def getNone(): return None
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     distill_url(r'^$', HomePageView.as_view(), name='home', distill_func=getNone),
     distill_url(r'^sobre/', AboutPageView.as_view(), name='about', distill_func=getNone),
     distill_url(r'^agenda/', CalendarPageView.as_view(), name='agenda', distill_func=getNone),
     distill_url(r'^opensource/', OpenSourcePageView.as_view(), name='opensource', distill_func=getNone),
     distill_url(r'^contato/', ContactPageView.as_view(), name='contact', distill_func=getNone),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
