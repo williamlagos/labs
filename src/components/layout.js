875d3cc54f8325eb8c57244767f262b06f3a27d6
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-11 16:57:41 -0300

    Added assets folder

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@
  */
 
 import React from "react"
+import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -25,21 +26,73 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Efforia Labs  </title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="./static/css/bootstrap.min.css" />
+        {/*<style>
+          body { padding-top: 50px; padding-bottom: 20px; }
+          @font-face {
+            font-family: 'Roboto';
+            src: url("./static/fonts/roboto-thin.ttf");
+          }
+          @font-face {
+            font-family: 'Roboto';
+            src: url("./static/fonts/roboto-bold.ttf");
+            font-weight: bold;
+          }
+        </style>*/}
+        <link rel="shortcut icon" href="./static/img/labs.png" />
+        <link rel="stylesheet" href="./static/css/main.css" />
+        <script src="./static/js/vendor/modernizr-2.8.3.min.js"></script>  
+      </Helmet>
+      <body>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <nav className="navbar navbar-blue navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Ativar navegação</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">
+                <img alt="Efforia Labs" src="./static/svg/labs.svg"/>
+              </a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="./agenda/">Eventos</a></li>
+                <li><a href="./opensource/">Código Aberto</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {children}
+        <footer className="footer">
+          <div className="container">
+            <h1 className="centered white"><div className="hidden-xs inline"> </div>+55 (51) 998 485 888</h1>
+            <h3 className="centered white" style={{ "font-size": "1.2em" }}><div className="hidden-xs inline ">E-mail: </div>labs@efforia.io</h3>
+            <p className="text-muted centered">Efforia © 2020</p>
+          </div>
         </footer>
-      </div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        {/*<script>window.jQuery || document.write('<script src="./static/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>*/}
+        <script src="./static/js/vendor/bootstrap.min.js"></script>
+        <script src="./static/js/main.js"></script>
+        {/*<script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+            </script>*/}
+      </body>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,40 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <div class="jumbotron">
+      <div class="container text-center">
+        <img class="img-fluid logo" src="./static/svg/labs_outline.svg"></img>
+        <h2>A próxima inspiração começa por aqui.</h2>
+        <p>Labs é um espaço para projetos experimentais na Efforia</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 centered">
+          <h2>Apollo</h2>
+          <h1><span class="label label-info">12/10</span></h1><h2></h2>
+          <img src="./static/svg/rocket.svg" width="90%" height="90%" class="rounded"/>
+          <p></p><p>Projeto de Landing Page e CMS.</p>
+        </div>
+        <div class="col-md-4 centered">
+          <h2>Cursos</h2>
+          <h1><span class="label label-info">Em Breve</span></h1><h2></h2>
+          <img src="./static/svg/hubpro.svg" width="90%" height="90%" class="rounded"/>
+          <p></p><p>Cursos de Design e Desenvolvimento.</p>
+        </div>
+        <div class="col-md-4 centered">
+          <h2>Dinamo</h2>
+          <h1><span class="label label-info">Em Breve</span></h1><h2></h2>
+          <img src="./static/svg/new.svg" width="90%" height="90%" class="rounded"/>
+          <p></p><p>Game de aventura em formato side-scrolling.</p>
+        </div>
+      </div>
+    </div>
+    <p></p><br/>
+
+
   </Layout>
 )
 
