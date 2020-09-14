3943ac7459cdd32e43cc34a9ed7a833f22f4aee0
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-14 19:01:41 -0300

    Commented out unused components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
-    <div class="jumbotron">
-      <div class="container text-center">
-        <img class="img-fluid logo" src="./static/svg/labs_outline.svg"></img>
+    <div className="jumbotron">
+      <div className="container text-center">
+        <img className="img-fluid logo" src="./static/svg/labs_outline.svg"></img>
         <h2>A próxima inspiração começa por aqui.</h2>
         <p>Labs é um espaço para projetos experimentais na Efforia</p>
       </div>
     </div>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 centered">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 centered">
           <h2>Apollo</h2>
-          <h1><span class="label label-info">12/10</span></h1><h2></h2>
-          <img src="./static/svg/rocket.svg" width="90%" height="90%" class="rounded"/>
+          <h1><span className="label label-info">12/10</span></h1><h2></h2>
+          <img src="./static/svg/rocket.svg" width="90%" height="90%" className="rounded"/>
           <p></p><p>Projeto de Landing Page e CMS.</p>
         </div>
-        <div class="col-md-4 centered">
+        <div className="col-md-4 centered">
           <h2>Cursos</h2>
-          <h1><span class="label label-info">Em Breve</span></h1><h2></h2>
-          <img src="./static/svg/hubpro.svg" width="90%" height="90%" class="rounded"/>
+          <h1><span className="label label-info">Em Breve</span></h1><h2></h2>
+          <img src="./static/svg/hubpro.svg" width="90%" height="90%" className="rounded"/>
           <p></p><p>Cursos de Design e Desenvolvimento.</p>
         </div>
-        <div class="col-md-4 centered">
+        <div className="col-md-4 centered">
           <h2>Dinamo</h2>
-          <h1><span class="label label-info">Em Breve</span></h1><h2></h2>
-          <img src="./static/svg/new.svg" width="90%" height="90%" class="rounded"/>
+          <h1><span className="label label-info">Em Breve</span></h1><h2></h2>
+          <img src="./static/svg/new.svg" width="90%" height="90%" className="rounded"/>
           <p></p><p>Game de aventura em formato side-scrolling.</p>
         </div>
       </div>
