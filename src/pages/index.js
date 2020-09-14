20cda2f8b2dbb023fdd42597e84c41f573cdd3da
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-14 19:05:33 -0300

    Removed warnings on images and headings on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
 
     <div className="jumbotron">
       <div className="container text-center">
-        <img className="img-fluid logo" src="./static/svg/labs_outline.svg"></img>
+        <img className="img-fluid logo" alt="Efforia Labs logo" src="./static/svg/labs_outline.svg"></img>
         <h2>A próxima inspiração começa por aqui.</h2>
         <p>Labs é um espaço para projetos experimentais na Efforia</p>
       </div>
@@ -21,20 +21,20 @@ const IndexPage = () => (
       <div className="row">
         <div className="col-md-4 centered">
           <h2>Apollo</h2>
-          <h1><span className="label label-info">12/10</span></h1><h2></h2>
-          <img src="./static/svg/rocket.svg" width="90%" height="90%" className="rounded"/>
+          <h1><span className="label label-info">12/10</span></h1>
+          <img src="./static/svg/rocket.svg" alt="Rocket launch" width="90%" height="90%" className="rounded"/>
           <p></p><p>Projeto de Landing Page e CMS.</p>
         </div>
         <div className="col-md-4 centered">
           <h2>Cursos</h2>
-          <h1><span className="label label-info">Em Breve</span></h1><h2></h2>
-          <img src="./static/svg/hubpro.svg" width="90%" height="90%" className="rounded"/>
+          <h1><span className="label label-info">Em Breve</span></h1>
+          <img src="./static/svg/hubpro.svg" alt="Efforia Labs logo" width="90%" height="90%" className="rounded"/>
           <p></p><p>Cursos de Design e Desenvolvimento.</p>
         </div>
         <div className="col-md-4 centered">
           <h2>Dinamo</h2>
-          <h1><span className="label label-info">Em Breve</span></h1><h2></h2>
-          <img src="./static/svg/new.svg" width="90%" height="90%" className="rounded"/>
+          <h1><span className="label label-info">Em Breve</span></h1>
+          <img src="./static/svg/new.svg" alt="Dinamo figure" width="90%" height="90%" className="rounded"/>
           <p></p><p>Game de aventura em formato side-scrolling.</p>
         </div>
       </div>
