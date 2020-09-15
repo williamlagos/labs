61ec8f91de6d65b47132b77642ee7bc4587de19f
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-15 17:22:29 -0300

    Removed and commented out unused components on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 // import { Link } from "gatsby"
-import { Header, Button, Anchor, Image } from "grommet"
+import { Header, Anchor, Image } from "grommet"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -7,7 +7,7 @@ import labsLogo from "../assets/svg/labs.svg"
 
 const Head = ({ siteTitle }) => (
   <Header background="brand">
-    <Button hoverIndicator />
+    {/* <Button hoverIndicator /> */}
     <Anchor href="/" icon={<Image src={labsLogo} />} />
     <Anchor href="./agenda/" label="Eventos" />
     <Anchor href="./opensource/" label="Código Aberto" />
