4f95a48cd0474134b44d24364c8c7844c46ee9df
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-16 18:07:42 -0300

    Changed some metadata for the website on gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Efforia Labs`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@efforiaio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
