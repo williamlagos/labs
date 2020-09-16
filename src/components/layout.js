cfb3fc0fb2f8a878a1b4c1e8ff68063ff42c9ab1
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-16 18:08:05 -0300

    Removed unused tags and elements on layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { Grommet, Footer, Heading, Paragraph } from 'grommet'
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import Head from "./header"
 
@@ -24,12 +24,10 @@ const Layout = ({ children }) => {
       <Helmet>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <title>Efforia Labs  </title>
-        <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href={labsLogo} />
       </Helmet>
-      <Head siteTitle={data.site.siteMetadata.title} />
+      <Head siteTitle="Efforia Labs" />
       {children}
       <Footer pad="medium" background="brand">
         <Heading className="centered white"><div className="hidden-xs inline"> </div>+55 (51) 998 485 888</Heading>
