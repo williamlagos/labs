a78dfedecf11872e8d53d43e4106ca2342b68e99
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-22 17:28:56 -0300

    Added close styling for header

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,13 @@ import React from "react"
 import labsLogo from "../assets/svg/labs.svg"
 
 const Head = ({ siteTitle }) => (
-  <Header background="brand">
-    {/* <Button hoverIndicator /> */}
-    <Box pad="small" height="xxsmall" width="xxsmall"><Image src={labsLogo} /></Box>
+  <Header background="brand" pad={{ "horizontal": "xsmall" }}>
+    <Box pad="small" height="xxsmall" width="xxsmall">
+      <Image fit="contain" src={labsLogo} />
+    </Box>
     <Box gap="small" direction="row" pad="small">
-      <Anchor href="./agenda/" label="Eventos" />
-      <Anchor href="./opensource/" label="Código Aberto" />
+      <Anchor size="small" color="white" href="./agenda/" label="EVENTOS" />
+      <Anchor size="small" color="white" href="./opensource/" label="CÓDIGO ABERTO" />
     </Box>
   </Header>
 )
