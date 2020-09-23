38f2a51a78132657ea65da9330f1bd13f821c1f2
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-23 19:23:54 -0300

    Added open source page

---
+++ src/pages/opensource.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import { Box, Heading, Paragraph, Image } from "grommet"
+import { Github } from "grommet-icons"
+
+const OpensourcePage = () => (
+    <Layout>
+      <SEO />
+      <Box align="center" pad="medium">
+        <Heading>Iniciativa Open Source da Efforia</Heading>
+        <Paragraph fill>
+          Nós acreditamos que para qualquer atividade é melhor executada quando coletivamente. Por isso 
+          a Efforia é incentivadora e contribuidora de projetos de Software Livre, mantendo um perfil 
+          próprio no GitHub e divulgando os projetos open source existentes para seus membros. A 
+          iniciativa torna-se completa com a existência da Efforia Labs, promovendo hackathons e 
+          dojos de forma a fomentar o código aberto.
+        </Paragraph>
+        <Box pad="medium" size="large" align="center" direction="row" gap="small" onClick={() => window.location = 'https://github.com/efforia'}>
+          <Github size="large" color="dark-1" />
+          <Heading level={2} margin="none">GitHub da Efforia</Heading>
+        </Box>
+      </Box>
+    </Layout>
+);
+
+export default OpensourcePage;
