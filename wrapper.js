5f9e22f1127f4fd9f326c2155b65d832ea846c8c
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-24 12:29:29 -0300

    Added redux wrapper on root repo

---
+++ wrapper.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+import { Provider } from "react-redux"
+
+import createAppState from "./src/state/"
+
+// eslint-disable-next-line react/display-name,react/prop-types
+export default ({ element }) => {
+  // Instantiating store in `wrapRootElement` handler ensures:
+  //  - there is fresh store for each SSR page
+  //  - it will be called only once in browser, when React mounts
+  const store = createAppState()
+  return <Provider store={store}>{element}</Provider>
+}
