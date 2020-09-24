592f72420802e825ed621732bcc37712cf265a2a
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-23 21:40:23 -0300

    Added redux store on layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,16 +6,22 @@
  */
 
 import React from "react"
-import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
 import { Grommet, Footer, Heading, Paragraph, Box } from 'grommet'
-// import { useStaticQuery, graphql } from "gatsby"
+import { Provider } from "react-redux"
+import { applyMiddleware, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
 
 import Head from "./header"
 
 import labsLogo from "../images/labs.png"
 
 import "./main.css"
+
+const store = createStore(() => {}, {}, composeWithDevTools(applyMiddleware(thunk, logger)))
 
 const landingTheme = {
   global: {
@@ -27,21 +33,23 @@ const landingTheme = {
 
 const Layout = ({ children }) => {
   return (
-    <Grommet theme={landingTheme}>
-      <Helmet>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href={labsLogo} />
-      </Helmet>
-      <Head siteTitle="Efforia Labs" />
-      {children}
-      <Footer pad="medium" direction="column" justify="center" background="dark-1">
-        <Heading level={2} margin="xxsmall">+55 (51) 998 485 888</Heading>
-        <Heading level={3} margin="xxsmall">labs@efforia.io</Heading>
-        <Paragraph margin="xxsmall">Efforia © 2020</Paragraph>
-      </Footer>
-    </Grommet>
+    <Provider store={store}>
+      <Grommet theme={landingTheme}>
+        <Helmet>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href={labsLogo} />
+        </Helmet>
+        <Head siteTitle="Efforia Labs" />
+        {children}
+        <Footer pad="medium" direction="column" justify="center" background="dark-1">
+          <Heading level={2} margin="xxsmall">+55 (51) 998 485 888</Heading>
+          <Heading level={3} margin="xxsmall">labs@efforia.io</Heading>
+          <Paragraph margin="xxsmall">Efforia © 2020</Paragraph>
+        </Footer>
+      </Grommet>
+    </Provider>
   )
 }
 
