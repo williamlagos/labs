c59413fe5c7e17ad3633061b75f081565d926f98
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-24 12:28:57 -0300

    Added state reducers and store to its source

---
+++ src/state/index.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+
+
+const reducer = (state, action) => {
+  if (action.type === `INCREMENT`) {
+    return Object.assign({}, state, {
+      count: state.count + 1,
+    })
+  }
+  return state
+}
+
+const initialState = { count: 0 }
+
+const createAppState = () => createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk, logger)))
+export default createAppState
