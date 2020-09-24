5a9481d072acda5129dd3628f2ed8c264fdb463e
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-24 11:19:43 -0300

    Removed warnings on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { Box, Heading, Paragraph, Image } from "grommet"
+import { Box, Heading, Paragraph, /* Image */ } from "grommet"
 
 const BlogPage = () => (
     <Layout>
